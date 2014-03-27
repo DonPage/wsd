f7bbd72e0edd491486b54b87b23c76fef0cee455
DonPage <don.page.work@gmail.com>
2014-03-27 12:33:22 -0400

    haven't validated yet

--- page-donald-final/js/main.js
+++ page-donald-final/js/main.js
@@ -1,6 +1,25 @@
 
 (function () {
+    console.log("js ready");
     $(document).ready(function (e) {
+        console.log("jq ready");
+        var login = document.querySelector('#login-button');
+        var inputUsername = document.querySelector('#Username');
+        var inputPassword = document.querySelector('#Userpass');
+        var user = document.querySelector('#user');
+
+
+        login.addEventListener('click',loginPressed);
+        function loginPressed(){
+            var usernameCharacters = inputUsername.value.length;
+            var passwordCharacters = inputPassword.value.length;
+            if (usernameCharacters > 0 && passwordCharacters > 0){
+                $(user).replaceWith('<p id="user">Welcome: '+inputUsername.value+'</p>');
+//                user.innerHTML = '<p id="userLoged">Welcome: '+inputUsername.value+'</p>';
+
+            }
+
+        }
 
 
     });
